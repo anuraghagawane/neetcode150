32325c70810f0318b19399a6f14e2d07f4a60fa0
anuraghagawane <anuraghagawane10@gmail.com>
2026-01-05 21:08:45 +0530

    Add solutions for array and hashing problems: Contains Duplicate, Group Anagrams, Two Sum, and Valid Anagram

---
+++ arrays & hashing/Group Anagrams.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(auto& s: strs) {
+            vector<int> count(26, 0);
+            for(auto ch: s) {
+                count[ch - 'a']++;
+            }
+
+            string key = to_string(count[0]);
+            for(int i = 1; i < 26; i++) {
+                key += "," + to_string(count[i]);
+            }
+
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for(auto m: mp) {
+            ans.push_back(m.second);
+        }
+
+        return ans;
+    }
+};
