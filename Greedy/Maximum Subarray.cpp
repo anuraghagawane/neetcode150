7a130c46ae903f7c75a0a876875875232202fdf7
anuraghagawane <anuraghagawane10@gmail.com>
2026-02-11 21:39:08 +0530

    Jump Game & maximum subarray

---
+++ Greedy/Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        for(auto num: nums) {
+            currSum = max(currSum + num, num);
+            maxSum = max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+};
